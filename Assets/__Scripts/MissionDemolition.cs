85ccc2bfd366e723a862127983448c7af12d2a01
Bill Crosbie <crosbie.git@gmail.com>
2019-09-15 21:54:46 -0400

    Initial commit

---
+++ Assets/__Scripts/MissionDemolition.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMode
+{
+
+
+}
+
+public class MissionDemolition : MonoBehaviour {
+    static private MissionDemolition S; // a private Singleton
+
+    [Header("Set in Inspector")]
+
+
+    [Header("Set Dynamically")]
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	void StartLevel()
+    {
+        // Get rid of the old castle if one exists
+ 
+    }
+
+    void UpdateGUI()
+    {
+
+    }
+
+    void Update()
+    {
+
+
+    }
+
+    void NextLevel()
+    {
+
+    }
+
+    public void SwitchView(string eView = "")
+    {
+
+    }
+
+    // Static method that allows code anywhere to increment shotsTaken
+    public static void ShotFired()
+    {
+
+    }
+}
---
+++ Assets/__Scripts/Slingshot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+  
+
+   
+     // fields set in the Unity Inspector pane
+    [Header("Set in Inspector")]
+
+
+    // fields set dynamically
+    [Header("Set Dynamically")]
+
+
+
+    private void Awake()
+    {
+
+    }
+
+    private void OnMouseEnter()
+    {
+
+    }
+
+    private void OnMouseExit()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+
+    private void Update()
+    {
+ 
+
+
+    }
+}
